dc4c57cc61cdf6b181e9a293e6390bceaeafe486
Sophie Kadletz <sophie.kadletz@htl-wels.at>
2021-01-12 20:59:24 +0100

    no-argument constructor added

--- src/model/CarDatabase.java
+++ src/model/CarDatabase.java
@@ -9,6 +9,21 @@ public class CarDatabase {
         this.db = db;
     }
 
+    public CarDatabase() {
+        MagicGenerator m = new MagicGenerator();
+
+        for(int i =0; i<1000; i++) {
+            String color = m.getRandomColor();
+            String owner = m.getRandomName();
+            String manufactor = m.getRandomManufacturer();
+            String model = m.getRandomModel(manufactor);
+            String licensePlate = m.getRandomLicencePlate();
+            Vehicle v = new Vehicle(color, owner, manufactor, model, licensePlate);
+            db.put(licensePlate, v);
+            System.out.println("Created vehicle with license plate " +licensePlate);
+        }
+    }
+
     public Vehicle[] search(String licencePlate, boolean exact) {
         return null;
     }
