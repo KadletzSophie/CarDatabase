05bd0306977daa869fcd6d6e659efd4c57702528
Sophie Kadletz <sophie.kadletz@htl-wels.at>
2021-01-12 20:45:38 +0100

    UML Diagram implemented

--- src/model/CarDatabase.java
+++ src/model/CarDatabase.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.HashMap;
+
 public class CarDatabase {
+    HashMap<String, Vehicle> db = new HashMap<>();
+
+    public CarDatabase(HashMap<String, Vehicle> db) {
+        this.db = db;
+    }
+
+    public Vehicle[] search(String licencePlate, boolean exact) {
+        return null;
+    }
 }
--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -1,4 +1,67 @@
 package model;
 
 public class Vehicle {
+    private String color;
+    private String owner;
+    private String maufacturing;
+    private String model;
+    private String licensePlate;
+
+    public Vehicle(String color, String owner, String maufacturing, String model, String licensePlate) {
+        this.color = color;
+        this.owner = owner;
+        this.maufacturing = maufacturing;
+        this.model = model;
+        this.licensePlate = licensePlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getMaufacturing() {
+        return maufacturing;
+    }
+
+    public void setMaufacturing(String maufacturing) {
+        this.maufacturing = maufacturing;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    @Override
+    public String toString() {
+        return "Manufactor: " + maufacturing + '\n' +
+                "Model: " + model + '\n' +
+                "Color: " + color + '\n' +
+                "Licence Plate: " + licensePlate + '\n' +
+                "Owner: " + owner + '\n';
+    }
+
 }
